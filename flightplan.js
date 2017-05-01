2802b310e66b9b5ea8edd5e83fcb6e73a54b76a5
Dylan  Lott <lott.dylan@gmail.com>
2017-04-30 22:55:51 -0600

    WIP flightplan

--- flightplan.js
+++ flightplan.js
@@ -7,9 +7,9 @@ plan.target('dev', {
 	host: '159.203.106.53',
   username: 'root',
   agent: process.env.SSH_AUTH_SOCK,
-	webRoot: '/var/www/dev.builtrightapp.com/server',
+	webRoot: '/var/www/dev.builtrightapp.com/client',
   ownerUser: 'root',
-  repository: 'https://github.com/dylanlott/builtright-api.git',
+  repository: 'https://github.com/dylanlott/builtright-vue.git',
   branchName: 'master',
   maxDeploys: 10
 });
@@ -18,65 +18,41 @@ plan.target('prod', {
   host: '159.203.106.53',
   username: 'root',
   agent: process.env.SSH_AUTH_SOCK,
-  webRoot: '/var/www/builtrightapp.com/server',
-  repository: 'https://github.com/dylanlott/builtright-api.git',
+  webRoot: '/var/www/builtrightapp.com/client',
+  repository: 'https://github.com/dylanlott/builtright-vue.git',
   branchName: 'master'
 });
 
-plan.remote('setup', function(remote) {
-  remote.hostname();
-  remote.with('cd ' + remote.runtime.webRoot, function() {
-    remote.sudo(`git clone ${remote.runtime.repository} .`);
-    remote.sudo('npm install');
-    remote.sudo('npm install -g pm2');
-    remote.sudo('pm2 start src/index.js');
-  })
-})
-
-//Git based deploy
-plan.local('deploy', function(local) {
-  local.hostname();
-  local.failsafe();
-  local.exec('git add . && git commit -am "flightplan push"');
-  local.log('Committed to GitHub');
-  local.exec('git push origin master');
-  local.log('Pushed to GitHub');
-  local.unsafe();
-});
-
-plan.remote('deploy', function(remote) {
-  remote.hostname();
-  remote.with('cd ' + remote.runtime.webRoot, function() {
-    remote.sudo('git pull origin master');
-    remote.sudo('npm install');
-    remote.failsafe();
-    remote.exec('pm2 restart src/index.js');
-    remote.unsafe();
-    remote.exec('pm2 list');
-    remote.log('Deploy successful');
-  });
-});
-
-//Docker based deploy
 plan.local('docker', function(local) {
   local.hostname();
-  local.exec('docker build -t hivemindapps/builtright-api .');
-  local.exec('docker push hivemindapps/builtright-api');
+	local.exec('npm run build');
+  local.exec('docker build -t hivemindapps/builtright-nginx .');
+  local.exec('docker push hivemindapps/builtright-nginx');
   local.log('Docker image built successfully and pushed to Docker Hub');
 });
 
 plan.remote('docker', function(remote) {
   remote.hostname();
-  remote.exec('docker pull hivemindapps/builtright-api:latest');
-  remote.failsafe();
-  remote.exec('docker stop builtright-api');
-  remote.unsafe();
-  remote.exec('docker run --name builtright-api -d -p 3030:3030 --link mongo:mongo hivemindapps/builtright-api:latest');
+  remote.exec('docker pull hivemindapps/builtright-nginx:latest');
+	remote.exec('docker restart builtright-nginx');
   remote.exec('docker ps');
-  remote.exec('docker logs builtright-api');
+  remote.exec('docker logs builtright-nginx');
 });
 
+plan.local('gitdeploy', function(local) {
+	local.hostname();
+	local.exec('git push origin master')
+});
+
+plan.remote('gitdeploy', function(remote) {
+	remote.hostname();
+	remote.with('cd /var/www/builtrightapp.com/client', function() {
+		remote.exec('git pull origin master');
+		remote.exec('npm install');
+		remote.exec('npm run build');
+	});
+})
+
 plan.remote('check', function(remote) {
-  remote.exec('pm2 list');
-  remote.exec('pm2 show builtright');
+  remote.exec('docker ps');
 });
